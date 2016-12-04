da55adb365005f85c616b4ca1b4eb47150e24c42
fahmisan <ahmadfahmisan@gmail.com>
2016-12-04 10:35:03 +0800

    small fix

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,12 +5,14 @@ class DashboardsController < ApplicationController
     if admin_user || staff_user
     @parcels = Parcel.where(status: 0)
     @shipments = Shipment.where(status: 0)
-    @todaysales = Shipment.where(status: 2, updated_at:DateTime.now)
+    @todaysale = Shipment.where(status: 2, updated_at:DateTime.now)
+    @todaysales = Shipment.where(status: 2, updated_at:DateTime.now).limit(10)
     @totalsales= Shipment.where(status:2)
     @activities = PublicActivity::Activity.order("created_at desc").limit(7)
     else
-      @totalsales= Shipment.where(status:2)
-      @todaysales = Shipment.where(status: 2, updated_at:DateTime.now)
+      @totalsales= current_user.shipments.where(status:2)
+      @todaysale = current_user.shipments.where(status: 2, updated_at:DateTime.now)
+      @todaysales = current_user.shipments.where(status: 2, updated_at:DateTime.now).limit(10)
     @parcels= current_user.parcels
     @shipments= current_user.shipments
     @activities = PublicActivity::Activity.order("created_at desc").where(owner_id: current_user).limit(7)
