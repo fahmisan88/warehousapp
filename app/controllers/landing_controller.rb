bd517e9974ca8182a454556aa43f74caa127f633
fahmisan <ahmadfahmisan@gmail.com>
2017-01-26 22:54:13 +0800

    fix link

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,8 +3,9 @@ class LandingController < ApplicationController
    def index
      if current_user && current_user.status == "active" && current_user.address?
        redirect_to dashboard_path
-     else
+     elsif current_user && current_user.status == "active" && current_user.address.empty?
        redirect_to edit_user_path(current_user)
+     else
      end
    end
  end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,14 @@ end
 def create
   user = User.find_by(email: user_params[:email])
              &.authenticate(user_params[:password])
-  if user && user.status == "Active"
+  if user && user.status == "Active" && user.address?
     session[:id] = user.id
     flash[:success] = "Welcome back #{current_user.name}"
     redirect_to dashboard_path
+  elsif user && user.status == "Active" && user.address.nil?
+    session[:id] = user.id
+    flash[:success] = "Please add your address before proceed"
+    redirect_to edit_user_path(user.id)
   elsif user && user.status == "Inactive"
     session[:id] = user.id
     flash[:success] = "Please pay our yearly fee to continue using our service"
