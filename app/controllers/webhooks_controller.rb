ba48146176c33f4305d88aa29ced484057d2c7af
Azlan Idris <azlandidie@yahoo.com>
2017-01-27 09:26:13 +0800

    fix ezi_id & days in a year

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -19,8 +19,8 @@ class WebhooksController < ApplicationController
 
   def user_payment_callback
     @user = User.find_by(bill_id: params[:id])
-    @ezi_id = @user.ezi_id + @user.id + 399
     @user.ezi_id = 0
+    @ezi_id = @user.ezi_id + @user.id + 399
     @expiry = @user.package * 365
     response = BillplzReg.check_status(@user.id)
     if (response['paid'] == true) && (response['state']=='paid')
