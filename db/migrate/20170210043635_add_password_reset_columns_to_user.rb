5d05b7dfc41d828094c8c96b81bfa0bdfc9caac8
Azlan Idris <azlandidie@yahoo.com>
2017-02-13 02:46:48 +0800

    resolved with migration

--- db/migrate/20170210043635_add_password_reset_columns_to_user.rb
+++ db/migrate/20170210043635_add_password_reset_columns_to_user.rb
@@ -2,7 +2,5 @@ class AddPasswordResetColumnsToUser < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :reset_password_token, :string
     add_column :users, :reset_password_sent_at, :datetime
-    remove_index :users, :password_reset_token
-    remove_column :users, :password_reset_token, :string
   end
 end
